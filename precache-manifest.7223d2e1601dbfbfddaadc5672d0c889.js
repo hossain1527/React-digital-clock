e227f8fe654495c901c1311512e139f01aa5b627
hossain1527 <hossain.sci2015@gmail.com>
2020-10-23 00:43:25 +0100

    Updates

---
+++ precache-manifest.7223d2e1601dbfbfddaadc5672d0c889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba13ebb726c783966befb11aca613d4f",
+    "url": "/React-digital-clock/index.html"
+  },
+  {
+    "revision": "62ebd7de69e4f04a9636",
+    "url": "/React-digital-clock/static/css/main.334a8144.chunk.css"
+  },
+  {
+    "revision": "fbda6c77cc1078e17399",
+    "url": "/React-digital-clock/static/js/2.af37030d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-digital-clock/static/js/2.af37030d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ebd7de69e4f04a9636",
+    "url": "/React-digital-clock/static/js/main.055bae4e.chunk.js"
+  },
+  {
+    "revision": "5c6586ddd48ebd3a23ec",
+    "url": "/React-digital-clock/static/js/runtime-main.81356882.js"
+  }
+]);
